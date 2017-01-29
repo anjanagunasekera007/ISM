9f1fc9562fafc58f01e6b3dcbecc1c6338cdb170
† ZEUS † < ZEUS >
2017-01-29 19:09:05 +0530

    Changes to R script

--- R/mba/grocerymining.R
+++ R/mba/grocerymining.R
@@ -19,14 +19,25 @@ ry <- apriori(Groceries,parameter = list(supp = 0.001,conf = 0.8))
 
 #show em
 options(digits = 2)
+print('Printing Basic assocation rules')
 print(inspect(ry[1:5]))
+print("/n")
+print("/n")
+
+
 #apassata kiyawapan 
 print("========================")
 roy <- sort(ry,by="confidence",decreasing = TRUE)
+print("Printing sorted association rules according to confidence level")
 print(roy)
+print("/n")
+print("/n")
+
+
 #print("aiyooooooooooooooooooo")
 print(",.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,")
 options(digits = 2)
+print("Printing for two digits of the sorted set")
 print(inspect(roy[1:35]))
 print(",.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,.,")
 
@@ -35,37 +46,44 @@ fileConn<-file("D:\\R\\mba\\FW.txt")
 fileConn2<-file("D:\\R\\mba\\Output.txt")
 writeLines(c("Hello","World"), fileConn)
 
+print("/n")
+print("/n")
+print("Commencing file writing off all association rules")
+
 write(roy, file = "D:\\FYP\\R\\mba\\newdata.csv", sep = ",")
 
 
 close(fileConn)
+print("/n")
+print("/n")
 
 #==================================
 
 #filter =3 lift none max value 11.5 
 #rules <- apriori(Groceries,parameter = list(support=0.001,conf=0.8, maxlen=3))
-
+print("Commencing Specialized association rule mining for -  whole milk - ")
 rah<-apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.08), 
                appearance = list(default="lhs",rhs="whole milk"),
                control = list(verbose=F))
 
 rah<-sort(rah, decreasing=TRUE,by="confidence")
-
+print("Prining Specialized association rule mining for -  whole milk - ")
 print(inspect(rah[1:5]))
 
 #print(rah)
 #sortedrah < -sort(rah, decreasing=TRUE,by="confidence")
 
-print("--------------------------------")
+#print("--------------------------------")
 #print(inspect(sortedrah[1:5]))
 
-
+print("Commencing Specialized association rule mining for -  whole milk - MINIMUM LENGTH OF RULE = 2")
 rere<-apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.15,minlen=2), 
-               appearance = list(default="rhs",lhs="yogurt"),
+               appearance = list(default="rhs",lhs="whole milk"),
                control = list(verbose=F))
 
 reresorted<-sort(rere, decreasing=TRUE,by="confidence")
 
+print("Prining Specialized association rule mining for -  whole milk - MINIMUM LENGTH OF RULE = 2")
 print(inspect(reresorted[1:5]))
 print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
 
