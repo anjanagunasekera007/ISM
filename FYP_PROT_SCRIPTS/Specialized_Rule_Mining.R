72a07028bc353251ef691bf98e650e64a4a717ec
† ZEUS † < ZEUS >
2017-02-11 20:41:49 +0530

    Specialized association rule mining

--- FYP_PROT_SCRIPTS/Specialized_Rule_Mining.R
+++ FYP_PROT_SCRIPTS/Specialized_Rule_Mining.R
@@ -1,31 +1,60 @@
+#Loading data
+library(arules)
+library(arulesViz)
+library(datasets)
+
+#load data set groceries 
+data(Groceries)
+
+item1 <- "whole milk"
+item2 <- "butter"
 #filter =3 lift none max value 11.5 
 #rules <- apriori(Groceries,parameter = list(support=0.001,conf=0.8, maxlen=3))
 print("Commencing Specialized association rule mining for -  whole milk - ")
-rah<-apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.08), 
-             appearance = list(default="lhs",rhs="whole milk"),
+wholemilk<-apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.08), 
+             appearance = list(default="lhs",rhs=item1),
              control = list(verbose=F))
 
-rah<-sort(rah, decreasing=TRUE,by="confidence")
+butter<-apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.08), 
+             appearance = list(default="lhs",rhs=item2),
+             control = list(verbose=F))
+
+wholemilk_sorted<-sort(wholemilk, decreasing=TRUE,by="confidence")
+butter_sorted<-sort(butter, decreasing=TRUE,by="confidence")
+
+
 print("Prining Specialized association rule mining for -  whole milk - ")
-print(inspect(rah[1:5]))
+print(inspect(wholemilk_sorted[1:5]))
+print("*****************************************************************************")
+print("Prining Specialized association rule mining for -  Butter - ")
+print(inspect(butter_sorted[1:5]))
 
-#print(rah)
-#sortedrah < -sort(rah, decreasing=TRUE,by="confidence")
 
-#print("--------------------------------")
-#print(inspect(sortedrah[1:5]))
+print("========== WRITING ============")
 
-print("Commencing Specialized association rule mining for -  whole milk - MINIMUM LENGTH OF RULE = 2")
-rere<-apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.15,minlen=2), 
-              appearance = list(default="rhs",lhs="whole milk"),
-              control = list(verbose=F))
+print("/n")
+print("/n")
+print("Commencing file writing of whole milk")
 
-reresorted<-sort(rere, decreasing=TRUE,by="confidence")
+write(wholemilk_sorted, file = "D:\\FYP\\FYP_PROT_SCRIPTS\\WHOLE_MILK.txt", sep = ",")
 
-print("Prining Specialized association rule mining for -  whole milk - MINIMUM LENGTH OF RULE = 2")
-print(inspect(reresorted[1:5]))
-print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+print("/n")
+print("/n")
 
-library(arulesViz)
-plot(reresorted,method="graph",interactive=TRUE,shading=NA)
+print("========== COMPLETED ============")
+
+
+
+print("========== WRITING ============")
+
+print("/n")
+print("/n")
+print("Commencing file writing of whole milk")
+
+write(butter_sorted, file = "D:\\FYP\\FYP_PROT_SCRIPTS\\BUTTER.txt", sep = ",")
+
+print("/n")
+print("/n")
+
+print("========== COMPLETED ============")
 
